59ea71df4c61038e6090c0d77f2b4ba65d92ed1d
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-03-30 10:17:29 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todo = [];
+let b = true;
+while (b){
+let a = await inquirer.prompt([
+  {
+    name: "list",
+    type: "input",
+    message: "What do you want to add into your Todos?",
+  },
+  {
+    name: "addmore",
+    type: "confirm",
+    message: "Do you want to add more into Todos",
+    default: "false",
+  },
+]);
+
+todo.push(a.list)
+b = a.addmore
+console.log(`Your Todos : ${todo}`);
+}
+
+
+
